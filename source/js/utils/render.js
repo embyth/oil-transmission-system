c3ad9b1dc8630bc2342a71ee1c7b998458e63808
Rostyslav Miniukov <thresh1337@gmail.com>
2020-12-22 23:47:19 +0200

    Додав рендер-методи. WIP

---
+++ source/js/utils/render.js
@@ -0,0 +1,47 @@
+import {RenderPosition} from '../const.js';
+import Abstract from '../view/abstract.js';
+
+export const render = (container, child, place) => {
+  if (container instanceof Abstract) {
+    container = container.getElement();
+  }
+
+  if (child instanceof Abstract) {
+    child = child.getElement();
+  }
+
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(child);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(child);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(child);
+      break;
+    case RenderPosition.BEFOREBEGIN:
+      container.before(child);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const remove = (component) => {
+  if (component === null) {
+    return;
+  }
+
+  if (!(component instanceof Abstract)) {
+    throw new Error(`Can remove only components`);
+  }
+
+  component.getElement().remove();
+  component.removeElement();
+};
