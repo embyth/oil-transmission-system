b071517b6873e929a1bad29b5cdc535085820ac1
Rostyslav Miniukov <thresh1337@gmail.com>
2020-12-22 23:48:09 +0200

    Додав константи. WIP

---
+++ source/js/const.js
@@ -0,0 +1,30 @@
+export const SHAKE_ANIMATION_TIMEOUT = 600;
+
+export const KeyCode = {
+  ENTER: 13,
+  ESC: 27
+};
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  AFTEREND: `afterend`,
+  BEFOREEND: `beforeend`,
+  BEFOREBEGIN: `beforebegin`,
+};
+
+export const SECTION = {
+  INTRO: `intro`,
+  DATA: `income-data`,
+  STATIONS: `income-stations`,
+  RESULTS: `results`,
+};
+
+export const ErrorMessage = {
+  VALUE_MISSING: `Введіть дані`,
+  RANGE_OVERFLOW: `Значення не може бути більше `,
+  RANGE_UNDERFLOW: `Значення не може бути менше `,
+};
+
+export const PIPE_ROUGHNESS = 0.2;
+
+export const END_SUPPORT = 30;
